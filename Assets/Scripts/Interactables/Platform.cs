56bacd8cbb56fa2f09043976e46728b47ddaaf25
Lukas Scheuerle <lukas.scheuerle@web.de>
2020-07-11 20:17:25 +0200

    basic platform movement example

--- Assets/Scripts/Interactables/Platform.cs
+++ Assets/Scripts/Interactables/Platform.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class Platform : Interactable
 {
+  Vector3 targetPosition;
+  int counter = 0;
   new void Start()
   {
     identifier = 'P';
     commands.Add("up", new Command(up));
+    commands.Add("down", new Command(down));
+    commands.Add("right", new Command(right));
+    commands.Add("left", new Command(left));
     base.Start();
+    targetPosition = transform.position;
   }
 
-  // Update is called once per frame
   void Update()
   {
-    
+    // Update position
+    // float x = Mathf.Lerp(this.transform.position.x, this.targetPosition.x, Time.deltaTime);
+    float y = Mathf.Lerp(this.transform.position.y, this.targetPosition.y, Time.deltaTime);
+    // float z = Mathf.Lerp(this.transform.position.z, this.targetPosition.z, Time.deltaTime);
+    this.transform.position = new Vector3(this.transform.position.x, y, this.transform.position.z);
   }
 
 
+  //TODO: probably make sure that the thing doesn't collide with other things
   private void up(string message)
   {
-    Debug.Log(this.gameObject);
+    targetPosition.y += commandWeight;
+  }
+  private void down(string message)
+  {
+    targetPosition.y -= commandWeight;
+  }
+  private void right(string message)
+  {
+    targetPosition.x += commandWeight;
+  }
+  private void left(string message)
+  {
+    targetPosition.x -= commandWeight;
   }
 }
