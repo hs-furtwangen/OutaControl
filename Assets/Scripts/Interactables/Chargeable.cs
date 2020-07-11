eb86d8b5645dbfc6265882fa95b4564e676bf832
Lukas Scheuerle <lukas.scheuerle@web.de>
2020-07-11 23:48:04 +0200

    First Chargeable Implementation

---
+++ Assets/Scripts/Interactables/Chargeable.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Chargeable : Interactable
+{
+  public LineRenderer ProgressBar;
+  protected float currentArmedStatus = 0.0f;
+  new void Start()
+  {
+    commands.Add(Cmd.arm, new Command(Arm));
+    commands.Add(Cmd.disarm, new Command(Disarm));
+    InteractableManager.instance.Register(this);
+  }
+
+  void Update()
+  {
+    if (currentlyActiveCooldown > 0)
+    {
+      currentlyActiveCooldown -= Time.deltaTime;
+    }
+    if (currentReactivationCooldown > 0)
+    {
+      currentReactivationCooldown -= Time.deltaTime;
+    }
+  }
+
+  protected void Arm(string message)
+  {
+    //TODO: add the possibility to "overheat"
+    if (currentlyActiveCooldown > 0 || currentReactivationCooldown > 0)
+    {
+      return;
+    }
+    currentArmedStatus += commandWeight;
+    Mathf.Clamp(currentArmedStatus, 0, 2);
+
+    if(currentArmedStatus > activationThreshold){
+      currentlyActiveCooldown = activeDuration;
+      Activate();
+    }
+    
+  }
+  protected void Disarm(string message)
+  {
+    if (currentlyActiveCooldown > 0 || currentReactivationCooldown > 0)
+    {
+      return;
+    }
+    currentArmedStatus -= commandWeight;
+    Mathf.Clamp(currentArmedStatus, 0, 2);
+  }
+  abstract protected void Activate();
+}
