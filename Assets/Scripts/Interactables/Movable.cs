57307a25b2e5c7f286adf0b1ce6ef4f38392f4c2
Moritz Roetner <moritz.roetner@ipm.fraunhofer.de>
2020-07-11 21:33:11 +0200

    "Fixed" Movable

--- Assets/Scripts/Interactables/Movable.cs
+++ Assets/Scripts/Interactables/Movable.cs
@@ -14,6 +14,8 @@ namespace Assets.Scripts.Interactables
         public Vector2 TargetPosition;
         public float Speed = 3f;
 
+        private float t = 0;
+
         private new void Start()
         {
             identifier = 'P';
@@ -27,25 +29,31 @@ namespace Assets.Scripts.Interactables
         private void Update()
         {
             if (transform.position.x <= TargetPosition.x && transform.position.y <= TargetPosition.y)
-                transform.position += new Vector3(MoveDirection.x * Speed * Time.deltaTime, MoveDirection.y * Speed * Time.deltaTime, 0);
+                t = t < 1 ? Speed * Time.deltaTime : 1;
+
+            transform.position = Vector3.Lerp(transform.position, TargetPosition, t);
         }
 
         //TODO: probably make sure that the thing doesn't collide with other things
         private void Up(string message)
         {
             TargetPosition.y += commandWeight;
+            t = 0;
         }
         private void Down(string message)
         {
             TargetPosition.y -= commandWeight;
+            t = 0;
         }
         private void Right(string message)
         {
             TargetPosition.x += commandWeight;
+            t = 0;
         }
         private void Left(string message)
         {
             TargetPosition.x -= commandWeight;
+            t = 0;
         }
     }
 }
