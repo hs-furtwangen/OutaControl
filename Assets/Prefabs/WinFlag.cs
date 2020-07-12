8a945cca30afa47f17b19f49890e47e8542a2d70
Emma Kemppainen <emma99.kem@gmail.com>
2020-07-12 20:44:51 +0200

    Winning Flag prefab

---
+++ Assets/Prefabs/WinFlag.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinFlag : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.CompareTag("Player")) {
+            GameLogic.WinningTeam = TEAM.GOOD;
+            GameLogic.State = GameState.GameOver;
+        }
+    }
