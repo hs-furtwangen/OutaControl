ad156a13e0bb29e6764ac6be303a7e28170887ac
Moritz Roetner <moritz.roetner@ipm.fraunhofer.de>
2020-07-12 16:43:27 +0200

    Added GameLogic Prefab

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
@@ -13,13 +14,17 @@ public enum GameState
     GameOver
 }
 
-public static class GameLogic
+public class GameLogic : MonoBehaviour
 {
     private static GameState _state;
+
+    public float TimeToWinInSeconds = 300;
 
     public static EventHandler<EventArgs> PreperationStateTriggered;
     public static EventHandler<EventArgs> PlayingStateTriggered;
     public static EventHandler<EventArgs> GameOverStateTriggered;
+
+    private static float gameStartTime;
 
     public static GameState State
     {
@@ -27,8 +32,8 @@ public static class GameLogic
         set
         {
             _state = value;
-            
-            switch(_state)
+
+            switch (_state)
             {
                 case GameState.Perperation:
                     PreperationStateTriggered?.Invoke(null, EventArgs.Empty);
@@ -43,10 +48,46 @@ public static class GameLogic
         }
     }
 
-    static GameLogic()
+    GameLogic()
     {
         PreperationStateTriggered += (s, e) => Debug.Log("PerperationStateTriggered");
         PlayingStateTriggered += (s, e) => Debug.Log("PlayingStateTriggered");
         GameOverStateTriggered += (s, e) => Debug.Log("GameOverStateTriggered");
     }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    public void StartGameTimer()
+    {
+        gameStartTime = Time.realtimeSinceStartup;
+        StartCoroutine(CheckForTimeout());
+    }
+
+    public void StopGameTimer()
+    {
+        StopCoroutine(CheckForTimeout());
+    }
+
+    public void ResetGameTimer()
+    {
+        StopGameTimer();
+        StartGameTimer();
+    }
+
+    private IEnumerator CheckForTimeout()
+    {
+        for (;;)
+        {
+            if (Time.realtimeSinceStartup - gameStartTime > TimeToWinInSeconds)
+            {
+                Debug.LogError("TIME IS OVER!");
+                State = GameState.GameOver;
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
 }
+
